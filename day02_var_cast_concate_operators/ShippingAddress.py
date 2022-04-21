54d87e70e11a1111b944d8a01a9ecf41d45679de
muhtar <muhtar@cydeo.com>
2022-04-20 20:56:16 -0400

    Python Programming: day02

---
+++ day02_var_cast_concate_operators/ShippingAddress.py
@@ -0,0 +1,27 @@
+
+"""
+Create a python file named ShippingAddress. Declare the following variables:
+                    name
+                    buildingNumber
+                    streetName
+                    city
+                    state
+                    zipCode
+
+    Use concatenation to print the full shipping address
+
+        Ex:
+            Given data:
+                name = "Aaron Kissinger"
+                buildingNumber = 13621A
+                streetName = "Legacy Circle"
+                city = "Fairfax"
+                state = "VA"
+                zipCode = 22030
+
+            Output:
+                Your shipping addrees is:
+                        Aaron Kissinger
+                        13621A Legacy Circle
+                        Fairfax, VA 22030
+"""
