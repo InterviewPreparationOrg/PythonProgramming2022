799ae65d5886105f559c18a5bd85917f0f83aaa5
muhtar <muhtar@cydeo.com>
2022-04-21 21:04:18 -0400

    Python Programming: day03

---
+++ day03_string_input_if/EmployeeInfo.py
@@ -0,0 +1,25 @@
+"""
+2. Create a python file named Employee Info:
+			Ask the user to enter the following info, and diplsay the user entered info:
+					 	name (String)
+		                age (integer)
+		                gender (String)
+		                company (String)
+		                job title (String)
+		                salary (float)
+
+            Ex:
+            	Given Data:
+            		name = "Daniel"
+            		age = 28
+            		gender = 'Male'
+					company_name = 'Google Inc'
+					job_title = "Scrum Master"
+					salary = 90000
+
+
+				Output:
+					Daniel is 28 years old, gender is Male
+					Daniel works at Google Inc as a Scrum Master
+					Daniel makes $ 90000 per year
+"""
