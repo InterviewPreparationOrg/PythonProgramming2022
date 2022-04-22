6dba969eb31b15ef817316f87eb45ce15976cc6e
muhtar <muhtar@cydeo.com>
2022-04-21 20:04:46 -0400

    Python Programming: day02

---
+++ day03_string_input_if/StringMethods.py
@@ -0,0 +1,69 @@
+
+name = 'CYDEO SCHOOL'
+
+name = name.lower()  # 'asel'
+
+print(name)
+
+print("------------------------------------")
+
+s1 = 'python programming language'
+s2 = s1.upper()
+
+print(s2)
+
+print("-------------------------------------")
+
+s3 = 'cyDEO'
+s3 = s3.capitalize()  # Cydeo
+
+print(s3)
+
+print("-------------------------------------")
+
+school_name = 'cyDEO schoOL'
+
+# school_name= school_name.capitalize()
+school_name = school_name.title() # Cydeo School
+
+print(school_name)
+
+tv_show = 'the walking dead'
+
+tv_show = tv_show.title()
+
+print(tv_show)
+
+print("-------------------------------------")
+
+str1 = '       Python         '
+str1 = str1.strip()
+
+print(len(str1))
+print(str1)
+
+print("-------------------------------------")
+
+word = 'Python Programming'
+
+i1 = word.index('g')
+print(i1)
+
+i2 = word.rindex('g')
+print(i2)
+
+print("-------------------------------------")
+
+sentence = 'I love python programming'
+
+result = sentence[sentence.index('p'):]
+
+print(result)
+
+print('---------------------------------------')
+
+sentence2 = 'python is the easiest programming language, I love python'
+
+print(sentence2.index('p'))
+print(sentence2.index('pr'))
+print(sentence2.rindex('p'))
