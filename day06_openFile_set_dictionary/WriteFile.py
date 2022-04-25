c1252bb9dfe453778127d253bb3a6a6e7f741d44
muhtar <muhtar@cydeo.com>
2022-04-25 19:35:39 -0400

    Python Programming: day06

---
+++ day06_openFile_set_dictionary/WriteFile.py
@@ -0,0 +1,5 @@
+file_path = '/Users/studio2/PycharmProjects/pythonProgramming2022/day06_openFile_set_dictionary/MyNotes2.txt'
+
+file = open(file_path, 'w')
+
+file.write('Hello Cybersecurity Batch 2 students\nHow are you today?')
