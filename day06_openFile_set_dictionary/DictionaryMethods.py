7b296fcee6130e75b8209f90a0643d16e2e1f650
muhtar <muhtar@cydeo.com>
2022-04-25 21:50:33 -0400

    Python Programming: day06

--- day06_openFile_set_dictionary/DictionaryMethods.py
+++ day06_openFile_set_dictionary/DictionaryMethods.py
@@ -25,3 +25,33 @@ print(employee1)
 employee1['salary'] = 150000
 
 print(employee1)
+
+employee1.pop('salary')
+
+print(employee1)
+
+employee1.popitem()
+
+print(employee1)
+
+employee1.popitem()
+
+print(employee1)
+
+employee1.popitem()
+
+print(employee1)
+
+print("---------------------------------------")
+
+
+student1 = {
+    'name': 'Josh',
+    'classes': ['Math', 'Art', 'Python']
+}
+
+print(student1['classes'])
+print(student1['classes'][1] )
+
+
+
